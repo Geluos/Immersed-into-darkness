636ea0141aec88a02db62299a01dd8e40de8ba9c
Chumakov123 <71515689+Chumakov123@users.noreply.github.com>
2021-04-11 18:16:55 +0300

    ScrollBarFix

---
+++ Assets/Scripts/UI/ScrollBar.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ScrollBar : MonoBehaviour
+{
+    public Scrollbar scrollBar;
+	public float koef;
+	public void Update()
+	{
+		scrollBar.value+=Input.GetAxis("Mouse ScrollWheel")*koef;
+		//if (scrollBar.value > 1) { scrollBar.value = 1; }
+		//if (scrollBar.value < 0) { scrollBar.value = 0; }
+
+	}
+}
