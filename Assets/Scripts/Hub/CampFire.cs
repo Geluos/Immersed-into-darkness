8213b39961c0e43ade36db653277a07bd50b722e
Chumakov123 <71515689+Chumakov123@users.noreply.github.com>
2021-04-11 21:47:20 +0300

    Modify CampFire

---
+++ Assets/Scripts/Hub/CampFire.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CampFire : MonoBehaviour
+{
+    public Light light;
+    private float iStart;
+    private float xStart;
+    private float yStart;
+    void Awake()
+    {
+        iStart = light.intensity;
+        xStart = transform.position.x;
+        yStart = transform.position.y;
+    }
+
+    void Update()
+    {
+        transform.position = new Vector2(xStart + Random.Range(-1, 1), yStart + Random.Range(-1, 1));
+        light.intensity = iStart + Random.Range(-3, 3);
+    }
+}
