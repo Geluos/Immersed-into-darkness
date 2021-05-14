8abca8d67353f8923c8b9e6779856ef083c354e3
ZeRo2281 <71148972+ZeRo2281@users.noreply.github.com>
2021-05-14 22:27:03 +0300

    Улучшил огонь в хабе(при нажатии)

---
+++ Assets/Scripts/FlameClick.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlameClick : MonoBehaviour
+{
+    public ParticleSystem ps;
+
+    private void OnMouseDown()
+    {
+        ps.Clear();
+        ps.Emit(350);
+    }
+
+}
